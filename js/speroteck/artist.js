bb82de18334925e5027f52694c3fa295954543f6
Alexei Savchenko <asavchenko@speroteck.com>
2013-07-25 18:14:19 +0300

    implemented line method

--- js/speroteck/artist.js
+++ js/speroteck/artist.js
@@ -45,27 +45,56 @@ Speroteck.Artist = Class.create({
      */
     lineColor: '#000',
 
+    lineObject: false,
+
     /**
      * initialization's operations go here
      */
-    initialize: function (options) {
+    initialize: function(options) {
         this.fabric = options.fabric;
         this.canvas = options.canvas;
         this.lineWidth = options.lineWidth || 5;
-        this.lineColor = options.lineColor || '#000';
+        this.lineColor = options.lineColor || '#000000';
     },
 
     /**
      *
-     * @param coords Array
+     * @param points Array
      * @returns {this.fabric.Line}
      */
-    line: function(coords) {
-        return new this.fabric.Line(coords, {
-            strokeWidth: this.strokeWidth,
-            stroke: this.strokeColor,
-            selectable: false
-        });
+    line: function(points) {
+        if (!this.lineObject) {
+            this.lineObject = new this.fabric.Line(points, {
+                strokeWidth: this.lineWidth,
+                stroke: this.lineColor,
+                selectable: false
+            });
+        }
+        this.lineObject.set('x1', points[0]);
+        this.lineObject.set('y1', points[1]);
+        this.lineObject.set('x2', points[2]);
+        this.lineObject.set('y2', points[3]);
+
+        this.canvas.add(this.lineObject);
+    },
+
+    /**
+     * not working!
+     * @param width Number
+     */
+    setLineWidth: function(width) {
+        this.lineWidth = width;
+    },
+
+    /**
+     * not working!
+     * format should be like:
+     * #000000
+     *
+     * @param color String
+     */
+    setLineColor: function(color) {
+       this.lineColor = color;
     }
 });
 
